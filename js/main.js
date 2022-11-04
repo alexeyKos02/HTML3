705e73824e8a89d03642942c7419aa82914fac51
alexeyKos02 <korolev.2002lexa@list.ru>
2022-11-04 15:03:01 +0300

    Адаптировал меню для мобильной версии

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@
         }
     }
 }());
+
+(function(){
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const menuClose = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', ()=>{
+        menu.classList.add('header_nav_active');
+    })
+    menuClose.addEventListener('click', ()=>{
+        menu.classList.remove('header_nav_active');
+    })
+}());
